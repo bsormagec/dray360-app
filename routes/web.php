f63320be96c244ef18ade2683584b43e48074bb3
Aaron Bryden <abryden@photector.com>
2020-03-10 16:01:39 -0500

    initial vue cli setup

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', 'VueController@index')->where('any', '.*');
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
