21c4cfafc2428ed58aac3b6d8c208cdd470b2b99
Aaron Bryden <abryden@photector.com>
2020-03-16 16:14:12 -0500

    initial vuetify with router setup (#2)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,37 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn>
+          <router-link to="/">Home</router-link>
+        </v-btn>
+        <v-btn>
+          <router-link to="/about">About</router-link>
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+   <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 
-
 export default {
   name: 'App',
+
   components: {
+  },
 
-  }
-}
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,20 +1,28 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import HelloWorld from '../components/HelloWorld.vue'
-import Test from '../components/Test.vue'
-Vue.use(Router);
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
 
 const routes = [
-    { path: '/test', component: HelloWorld },
-    { path: '/test2', component: Test }
-  ]
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
 
-  // 3. Create the router instance and pass the `routes` option
-  // You can pass in additional options here, but let's
-  // keep it simple for now.
-  const router = new Router({
-    mode: 'history',
-    routes // short for `routes: routes`
-  });
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
-  export default router;
+export default router
