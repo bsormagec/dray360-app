9056a5e1cf93c6ca9e0f36763dce3f48d05f78dd
Daniel Sarmient Cordero <dsarmiento@draymaster>
2020-03-20 16:41:40 -0500

    Override table styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -18,4 +18,40 @@ html {
 body {
   font-size: 1.6rem;
 }
+
+h1 {
+  font-size: 2.6rem;
+}
+
+h2 {
+  font-size: 1.6rem;
+}
+
+div,
+span,
+p,
+label,
+td {
+  font-size: 1.2rem !important;
+  color: map-get($colors, black);
+}
+
+.v-data-table {
+  border-radius: unset;
+  box-shadow: map-get($properties, table-shadow);
+
+  * {
+    border: unset !important;
+  }
+
+  td {
+    border: 0.1rem solid #dadddd;
+    border-right: unset;
+    border-bottom: unset;
+  }
+
+  .v-data-footer {
+    border: unset !important;
+  }
+}
 </style>
