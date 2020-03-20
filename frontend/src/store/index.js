23c40c0782aed4efb528b290335e7e1471458dbe
Daniel Sarmient Cordero <dsarmiento@draymaster>
2020-03-20 17:03:12 -0500

    Add axios and initial store module orders

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import orders from '@/store/modules/orders'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  modules: {
+    orders
+  }
 })
---
+++ frontend/src/store/modules/orders.js
@@ -0,0 +1,17 @@
+const mutations = {
+  setOrders (state, orders) {
+    state = orders
+  }
+}
+
+const actions = {
+  getOrders ({ commit }, params) {
+    // api call
+  }
+}
+
+export default {
+  state: [],
+  mutations,
+  actions
+}
