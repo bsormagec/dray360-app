77b5cf9b246e1f6498274af09368bd620fc16bb5
Daniel Sarmiento Cordero <dsarmiento@lean-tech.io>
2020-03-30 17:17:50 -0500

    Add utils/for_tests/render_with_vuetify.js (it adds vuetify to the render method of testing-library/vue)

--- frontend/src/views/Orders/__tests__/2.listFetching.js
+++ frontend/src/views/Orders/__tests__/2.listFetching.js
@@ -1,4 +1,5 @@
-import { render, waitFor } from '@testing-library/vue'
+import { waitFor } from '@testing-library/vue'
+import renderWithVuetify from '@/utils/for_tests/render_with_vuetify'
 import elNotFound from '@/utils/for_tests/el_not_found'
 import authTestUser from '@/utils/for_tests/auth_test_user'
 import Orders from '@/views/Orders/Orders'
@@ -8,13 +9,13 @@ export default () =>
   describe('listFetching', () => {
     it('doesnt render list items before fetch', async () => {
       await authTestUser()
-      const { getByTestId } = render(Orders, { store })
+      const { getByTestId } = renderWithVuetify(Orders, { store })
       expect(elNotFound({ getFn: getByTestId, elTestId: 'test-list' })).toBeTruthy()
     })
 
     it('renders list items after fetch', async () => {
       await authTestUser()
-      const { getByTestId } = render(Orders, { store })
+      const { getByTestId } = renderWithVuetify(Orders, { store })
       const listTable = await waitFor(() =>
         getByTestId('test-list')
       )
