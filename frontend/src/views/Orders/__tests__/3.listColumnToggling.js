0349ea6f324634f182d8fcf553494efd5fa145b6
Daniel Sarmiento Cordero <dsarmiento@lean-tech.io>
2020-04-01 22:05:48 -0500

    Add listColumnToggling tests

--- frontend/src/views/Orders/__tests__/3.listColumnToggling.js
+++ frontend/src/views/Orders/__tests__/3.listColumnToggling.js
@@ -1,24 +1,47 @@
-// import { fireEvent } from '@testing-library/vue'
-// import Orders from '@/views/Orders/Orders'
-// import store from '@/store/modules/orders'
-// import renderAuthenticated from '@/utils/for_tests/render_authenticated'
-// import endQeue from '@/utils/for_tests/end_qeue'
+import Orders from '@/views/Orders/Orders'
+import store from '@/store/modules/orders'
+import mount from '@/utils/for_tests/mount_authenticated'
+import waitForResponse from '@/utils/for_tests/wait_for_response.js'
 
 export default () =>
   describe('listColumnToggling', () => {
     it('hides a column', async () => {
-      // const { container } = await renderAuthenticated(Orders, { store })
-      // const dropdown = container.querySelector('.header__dropdown div[role="button"]')
+      const wrapper = await mount(Orders, { store })
+      await waitForResponse.wait()
 
-      // fireEvent.click(dropdown)
-      // await endQeue()
+      wrapper.find('.header__dropdown div[role="button"]').trigger('click')
+      await wrapper.vm.$nextTick()
+      wrapper.find('[role="listbox"] div:first-child').trigger('click')
+      await wrapper.vm.$nextTick()
 
-      // const menu = container.querySelector('.v-menu__content div[role="listbox"]')
-      // const firstOption = menu.children[0]
-      // fireEvent.click(firstOption)
-      // await endQeue()
+      const firstOpText = wrapper.find('div[role="listbox"] div:first-child').text()
+      const headerCell = wrapper.find(`th[aria-label="${firstOpText}"]`)
+      expect(headerCell.exists()).toBe(false)
+    })
 
-      // const columns = container.querySelectorAll('th[aria-label="Id"]')
-      // console.log(columns)
+    it('shows previously hidden column', async () => {
+      const wrapper = await mount(Orders, { store })
+      await waitForResponse.wait()
+
+      wrapper.find('.header__dropdown div[role="button"]').trigger('click')
+      await wrapper.vm.$nextTick()
+      wrapper.find('div[role="listbox"] div:first-child').trigger('click')
+      await wrapper.vm.$nextTick()
+      /*
+        Click again same option to show previously hidden column
+      */
+      wrapper.find('div[role="listbox"] div:first-child').trigger('click')
+      await wrapper.vm.$nextTick()
+      /* end */
+
+      const firstOpText = wrapper.find('div[role="listbox"] div:first-child').text()
+      const headerCell = wrapper.find(`th[aria-label="${firstOpText}"]`)
+      expect(headerCell.exists()).toBe(true)
     })
   })
+
+function sleep () {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, 500)
+  })
+}
