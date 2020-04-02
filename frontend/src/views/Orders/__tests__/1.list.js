33406c30567e70737aaf2a1ba0a97d7a7a020226
Daniel Sarmiento Cordero <dsarmiento@lean-tech.io>
2020-04-02 12:36:20 -0500

    Add listPagination tests for views/Orders

--- frontend/src/views/Orders/__tests__/1.list.js
+++ frontend/src/views/Orders/__tests__/1.list.js
@@ -1,8 +1,10 @@
 import listFetching from './2.listFetching'
 import listColumnToggling from './3.listColumnToggling'
+import listPagination from './4.listPagination'
 
 export default () =>
   describe('list', () => {
     listFetching()
     listColumnToggling()
+    listPagination()
   })
