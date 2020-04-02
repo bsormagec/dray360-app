107f569228fa90254e516201e783465be1d02037
Daniel Sarmiento Cordero <dsarmiento@lean-tech.io>
2020-04-02 12:35:52 -0500

    Set NUM_ORDERS_TO_CREATE = 100 in database/seeds/OrdersTableSeeder.php

--- database/seeds/OrdersTableSeeder.php
+++ database/seeds/OrdersTableSeeder.php
@@ -12,7 +12,7 @@ class OrdersTableSeeder extends Seeder
     // class constants
 
     // define how many to seed
-    const NUM_ORDERS_TO_CREATE = 10;
+    const NUM_ORDERS_TO_CREATE = 100;
     const PCT_ORDERS_INTAKE_REJECTED = 15;
 
     // lists of sample data values
