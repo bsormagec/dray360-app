cfda8b840a06b431409d01b8ed0212d4be333dd8
Daniel Sarmiento Cordero <dsarmiento@lean-tech.io>
2020-04-03 20:27:22 -0500

    Refactor OrdersCreate and add OrdersCreateSubmitted and OrdersCreateUpload

--- frontend/src/views/Orders/OrdersCreate.vue
+++ frontend/src/views/Orders/OrdersCreate.vue
@@ -10,67 +10,15 @@
       </v-btn>
     </div>
 
-    <div class="create__upload">
-      <h1 class="upload__title">
-        CREATE ORDER BY UPLOAD
-      </h1>
-      <h2 class="upload__subtitle">
-        Upload Documents
-      </h2>
+    <OrdersCreateUpload
+      :files="files"
+      :handle-drop="handleDrop"
+    />
 
-      <div class="upload__input">
-        <v-file-input
-          v-model="files"
-          multiple
-          solo
-          dense
-          hide-details
-          append
-          accept=".pdf"
-          label="File input"
-        />
-
-        <span class="input__legend">
-          <strong>Supported file type:</strong> PDF
-        </span>
-      </div>
-
-      <div
-        class="upload__area"
-        @dragenter.prevent.stop
-        @dragover.prevent.stop
-        @dragleave.prevent.stop
-        @drop.prevent.stop="handleDrop"
-      >
-        <span class="area__legend">... or drag documents here to upload</span>
-        <v-file-input
-          v-model="files"
-          multiple
-          accept=".pdf"
-        />
-      </div>
-    </div>
-
-    <div class="create__submitted">
-      <h2 class="submitted__title">
-        Submitted Documents
-      </h2>
-
-      <div
-        v-for="(file, i) in files"
-        :key="i"
-        class="submitted__file"
-      >
-        <v-icon>mdi-file-outline</v-icon>
-        <span>{{ file.name }}</span>
-        <v-icon
-          color="red"
-          @click="deleteFile(file)"
-        >
-          mdi-delete
-        </v-icon>
-      </div>
-    </div>
+    <OrdersCreateSubmitted
+      :files="files"
+      :delete-file="deleteFile"
+    />
 
     <v-btn
       color="primary"
@@ -82,8 +30,16 @@
 </template>
 
 <script>
+import OrdersCreateUpload from '@/views/Orders/OrdersCreateUpload'
+import OrdersCreateSubmitted from '@/views/Orders/OrdersCreateSubmitted'
+
 export default {
   name: 'OrdersCreate',
+
+  components: {
+    OrdersCreateUpload,
+    OrdersCreateSubmitted
+  },
 
   data: () => ({
     files: []
@@ -122,70 +78,5 @@ export default {
 .header__btn {
   margin: 0 auto;
   margin-bottom: 3.2rem;
-}
-
-.create__upload {
-  display: flex;
-  flex-direction: column;
-  padding-top: 2.8rem;
-  margin-bottom: 3.4rem;
-}
-
-.upload__title {
-  font-size: 2.1rem;
-  line-height: 2.4rem;
-  margin-bottom: 3.4rem;
-}
-
-.upload__subtitle {
-  line-height: 1.8rem;
-  margin-bottom: 0.9rem;
-}
-
-.upload__input {
-  margin-bottom: 2rem;
-}
-
-.upload__input .input__legend {
-  display: block;
-  margin-top: 0.8rem;
-  color: map-get($colors, grey-4);
-}
-
-.upload__area {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 9.4rem;
-  background-color: map-get($colors, grey-5);
-  border: 0.2rem dashed map-get($colors, grey-3);
-  border-radius: 0.5rem;
-}
-
-.area__legend {
-  display: block;
-  font-size: 1.4rem !important;
-  font-weight: bold;
-  color: map-get($colors, grey-4);
-}
-
-.submitted__title {
-  border-bottom: 0.1rem solid map-get($colors, grey-2);
-  margin-bottom: 1.7rem;
-}
-
-.submitted__file {
-  display: flex;
-  align-items: center;
-  margin-bottom: 2.4rem;
-
-  span {
-    margin-left: 0.7rem;
-    margin-right: 1.9rem;
-    font-size: 1.4rem !important;
-    font-weight: bold;
-    color: map-get($colors, blue);
-    text-decoration-line: underline;
-  }
 }
 </style>
