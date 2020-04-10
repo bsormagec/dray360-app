57c07315531a1295b7c1510e465ed9fc3b710929
Daniel Sarmiento Cordero <dsarmiento@lean-tech.io>
2020-04-09 19:16:07 -0500

    Add initial Details view

---
+++ frontend/src/views/Details/Details.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="details"
+  >
+    <DetailsSidebar />
+  </div>
+</template>
+
+<script>
+import DetailsSidebar from '@/views/Details/DetailsSidebar'
+
+export default {
+  name: 'Details',
+
+  components: {
+    DetailsSidebar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.details {
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
+</style>
---
+++ frontend/src/views/Details/DetailsSidebar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div
+    class="sidebar"
+  >
+    <div class="sidebar__logo" />
+    <div class="sidebar__footer" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailsSidebar'
+}
+</script>
+
+<style lang="scss" scoped>
+$cushing-logo: url("../../assets/images/cushing_logo.svg");
+$ordermaster-logo: url("../../assets/images/ordermaster_logo.svg");
+
+.sidebar {
+  z-index: 1;
+  display: flex;
+  position: fixed;
+  width: map-get($sizes, sidebar-desktop-width);
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  background-color: map-get($colors, grey);
+  box-shadow: map-get($properties, inset-shadow-right);
+  padding-top: 4rem;
+  padding-bottom: 3rem;
+}
+
+.sidebar__logo {
+  width: 72%;
+  max-width: 14rem;
+  height: 4.3rem;
+  background-image: $cushing-logo;
+  background-size: contain;
+  background-position: center center;
+}
+
+.sidebar__footer {
+  width: 57%;
+  max-width: 11.1rem;
+  height: 4.5rem;
+  margin-top: auto;
+  background-image: $ordermaster-logo;
+  background-size: contain;
+  background-position: center center;
+}
+</style>
