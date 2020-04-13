a5f60ebbbabc1bb3b04862c0d8f2b2e58926dd1f
Daniel Sarmiento Cordero <dsarmiento@lean-tech.io>
2020-04-13 18:04:08 -0500

    Add bg color to views/Details/DetailsDocument.vue

--- frontend/src/views/Details/DetailsDocument.vue
+++ frontend/src/views/Details/DetailsDocument.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="document">
-    document
-  </div>
+  <div class="document" />
 </template>
 
 <script>
@@ -9,3 +7,12 @@ export default {
   name: 'DetailsDocument'
 }
 </script>
+
+<style lang="scss" scoped>
+.document {
+  height: 100%;
+  display: flex;
+  flex-grow: 1;
+  background: map-get($colors , grey-8);
+}
+</style>
