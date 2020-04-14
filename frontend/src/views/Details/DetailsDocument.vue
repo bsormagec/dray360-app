7357d487a5d95ffea0b70fc7c47c5affedafe403
Daniel Sarmiento Cordero <dsarmiento@lean-tech.io>
2020-04-14 09:53:02 -0500

    Add styles to views/Details/DetailsDocument.vue

--- frontend/src/views/Details/DetailsDocument.vue
+++ frontend/src/views/Details/DetailsDocument.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="document" />
+  <div class="document">
+    <img
+      v-for="(page, i) in pages"
+      :key="i"
+      class="document__page"
+      :src="page"
+      alt="document page"
+    >
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'DetailsDocument'
+  name: 'DetailsDocument',
+
+  data: () => ({
+    pages: [
+      'https://www.freightquote.com/en-US/-/media/Freightquote/Resources/bol_website_2016.jpg?h=716&w=750&la=en-US'
+    ]
+  })
 }
 </script>
 
@@ -13,6 +27,14 @@ export default {
   height: 100%;
   display: flex;
   flex-grow: 1;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 2.6rem 0;
   background: map-get($colors , grey-8);
+}
+
+.document__page {
+  width: 90%;
+  object-fit: contain;
 }
 </style>
