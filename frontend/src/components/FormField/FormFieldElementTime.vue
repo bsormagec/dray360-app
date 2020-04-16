0714020893fdd6b078f7fc904b3cf9fe1f69b5bf
Daniel Sarmiento Cordero <dsarmiento@lean-tech.io>
2020-04-16 14:45:23 -0500

    Add components/FormField/FormFieldElementTime.vue

--- frontend/src/components/FormField/FormFieldElementTime.vue
+++ frontend/src/components/FormField/FormFieldElementTime.vue
@@ -1,30 +1,19 @@
-/*
-  TODO: Timepicker mask but is an input
-*/
-
 <template>
-  <div class="form-field-element-time">
-    <v-dialog
-      v-model="modal2"
-      width="290px"
-    >
-      <template v-slot:activator="{ on }">
-        <v-text-field
-          v-model="time"
-          label="Picker in dialog"
-          prepend-icon="mdi-clock-outline"
-          readonly
-          v-on="on"
-        />
-      </template>
+  <div :class="{'form-field-element-time': true, focused: focused}">
+    <v-icon>
+      mdi-clock-outline
+    </v-icon>
 
-      <v-time-picker
-        v-if="modal2"
+    <div :class="{time__mask: true, focused: focused}">
+      <span :class="{mask__label: true, focused: focused, filled: Boolean(time)}">label</span>
+      <input
         v-model="time"
-        full-width
-        @input="modal2 = false"
-      />
-    </v-dialog>
+        class="mask__input"
+        type="text"
+        @focus="toggleFocused"
+        @blur="change"
+      >
+    </div>
   </div>
 </template>
 
@@ -40,8 +29,77 @@ export default {
   },
 
   data: () => ({
-    modal2: false,
-    time: undefined
-  })
+    time: '',
+    focused: false
+  }),
+
+  methods: {
+    toggleFocused () {
+      this.focused = !this.focused
+    },
+
+    change () {
+      this.toggleFocused()
+      this.$emit('change', this.time)
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.form-field-element-time {
+  display: flex;
+  align-items: flex-start;
+  height: 6.6rem;
+  padding-top: 1.6rem;
+
+  .v-icon {
+    margin-top: 0.4rem;
+    margin-bottom: 0.4rem;
+    margin-right: 0.9rem;
+  }
+
+  &.focused .v-icon {
+    color: map-get($colors , blue);
+  }
+}
+
+.time__mask {
+  position: relative;
+  height: 3.2rem;
+  border-width: 0.1rem;
+  width: 100%;
+  box-shadow: inset 0 -0.1rem 0 0 map-get($colors , grey-10);
+  transition: box-shadow 200ms ease-in-out;
+
+  &.focused {
+    border-width: 0.2rem;
+    box-shadow: inset 0 -0.1rem 0 0 map-get($colors , blue);
+  }
+}
+
+.mask__label {
+  display: block;
+  margin-top: 0.8rem;
+  color: map-get($colors , black-2);
+  text-transform: capitalize;
+  transition: color 200ms ease-in-out, transform 200ms ease-in-out;
+
+  &.filled {
+    transform: translateY(-1.8rem) !important;
+  }
+
+  &.focused {
+    color: map-get($colors , blue);
+    transform: translateY(-1.8rem) !important;
+  }
+}
+
+.mask__input {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  outline: none;
+}
+</style>
