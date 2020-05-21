b7f24fee8257a143fc5face016cd9b31816b10e4
mauricio <mmontoya@lean-tech.io>
2020-05-21 17:09:05 -0500

    clean appearance

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,43 +1,70 @@
 <template>
   <div>
-    This is a login page . . <br>
-    <input
-      v-model="email"
-      type="text"
-      name="username"
-      placeholder="Email"
-    >
-    <br>
-    <input
-      v-model="password"
-      type="password"
-      name="password"
-      placeholder="Password"
-    >
-    <button
-      type="button"
-      @click="login()"
-    >
-      Login
-    </button>
-    <br><div v-if="loginError">
-      There was a problem. Please check your email and password.
+    <div class="wrapper">
+      <div class="login-box">
+        <!-- <v-text-field
+          v-model="email"
+          type="text"
+          name="username"
+          placeholder="Email"
+          class="login_textfields"
+        /> -->
+        <FormFieldElementInput
+          v-model="email"
+          :field="fields_email"
+        />
+        <FormFieldElementInput
+          v-model="password"
+          :field="fields_password"
+        />
+        <!-- <v-text-field
+          v-model="password"
+          type="password"
+          name="password"
+          placeholder="Password"
+          class="login_textfields"
+        /> -->
+        <div class="button_checkbox">
+          <v-checkbox
+            v-model="disabled"
+            class="mx-2"
+            label="Remember me"
+          />
+          <button
+            type="button"
+            class="btn-login"
+            @click="login()"
+          >
+            Login
+          </button>
+        </div>
+      </div>
+      <div class="account">
+        <p><span class=""><a href="">Forgot your password?</a></span></p>
+        <p><span><a href="">Don't have an account?</a></span></p>
+      </div>
+      <br><div v-if="loginError">
+        There was a problem. Please check your email and password.
+      </div>
     </div>
   </div>
 </template>
-
 <script>
 // import axios from '@/store/api_calls/axios_config'
-
+import FormFieldElementInput from '@/components/FormField/FormFieldElementInput'
 export default {
   name: 'Login',
+  components: {
+    FormFieldElementInput
+  },
   data () {
     return {
-
+      disabled: false,
       email: '',
       password: '',
-      loginError: false
-
+      loginError: false,
+      fields_email: { name: 'email', label: 'Email', placeholder: 'Email', el: { value: '' } },
+      fields_password: { name: 'Password', type: 'password', label: 'Password', placeholder: 'Password', el: { value: '' } }
     }
   },
   methods: {
@@ -55,3 +82,58 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+  $background_login: url("../assets/images/login_background.png");
+  .wrapper{
+    background: $background_login no-repeat center center fixed;
+    background-size: cover;
+    display: flex;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    .login-box{
+      width: 360px;
+      justify-items: center;
+      background-color: #fff;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      border-top: 8px solid #397E92;
+      padding: 30px;
+      .form-field-element-input{
+           width: 100%;
+      }
+      .button_checkbox{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-around;
+        font-weight: bold;
+        width: 100%;
+        .btn-login{
+          padding: 5px 40px;
+          background-color: #397E92;
+          color: white;
+          border-radius: 3px;
+        }
+      }
+    }
+    .account{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        width: 360px;
+        border-top: 1px solid #E4E8E9;
+        background-color: white;
+        padding-top: 20px;
+        padding: 10px 10px 0px 10px;
+      }
+  }
+</style>
