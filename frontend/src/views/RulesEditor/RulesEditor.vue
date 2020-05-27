4680114632318734dfe77f523e76f1dc9111066e
Esteban <esteban@lean-tech.io>
2020-05-27 15:57:44 -0500

    Add rule editing

--- frontend/src/views/RulesEditor/RulesEditor.vue
+++ frontend/src/views/RulesEditor/RulesEditor.vue
@@ -6,37 +6,54 @@
       Add Rule
     </v-btn>
     <draggable
-      v-model="codeSnippets"
+      v-model="rules_array"
       group="snippets"
       @start="drag=true"
       @end="drag=false"
     >
       <div
-        v-for="(element, index) in codeSnippets"
+        v-for="(element, index) in rules_array"
         :key="element.id"
         class="snippet-div"
       >
+        <h2>
+          {{ element.name }}
+        </h2>
+        <v-btn
+          @click="editRuleName(index)"
+        >
+          Edit Rule Name
+        </v-btn>
         <v-btn
           @click="removeSnippet(index)"
         >
           Remove Rule
         </v-btn>
+        <v-btn
+          @click="testSnippet(index)"
+        >
+          Test
+        </v-btn>
         <codemirror
           ref="cmEditor"
-          v-model="codeSnippets[index]"
+          v-model="rules_array[index].code"
           :options="cmOptions"
           @input="onCmCodeChange(index)"
         />
         <!-- Rule description -->
         <div>
-          Sums up two numbers. Prints the result
+          {{ element.description }}
+          <v-btn
+            @click="editRuleDescription(index)"
+          >
+            Edit Rule Description
+          </v-btn>
         </div>
       </div>
     </draggable>
   </div>
 </template>
 <script>
-/* eslint-disable vue/valid-v-model */
 import draggable from 'vuedraggable'
 import { codemirror } from 'vue-codemirror'
 import 'codemirror/lib/codemirror.css'
@@ -48,31 +65,60 @@ export default {
     codemirror
   },
   data: () => ({
-    code: 'Hello world',
-    codeSnippets: ["# This program adds two numbers\nnum1 = 1.5\nnum2 = 6.3\n# Add two numbers\nsum = num1 + num2\n\n# Display the sum\nprint('The sum of {0} and {1} is {2}'.format(num1, num2, sum)", 'if (year % 4) == 0:\n\tif (year % 100) == 0:'],
     cmOptions: {
       tabSize: 4,
       mode: 'text/x-python',
       theme: 'base16-light',
       lineNumbers: true,
       line: true
-    }
+    },
+    // Mocked rules
+    rules_array: [
+      {
+        name: 'Rule 1',
+        description: 'Mocked description for Rule 1',
+        code: 'possible_output_fields = {"shipment_type":{"always":True}}\ndef runrule(input_fields, updated_fields):\n\t#return {"shipment_type":"export"}\n\tupdated_fields["shipment_type"] = "export"'
+      },
+      {
+        name: 'Rule 2',
+        description: 'Mocked description for Rule 2',
+        code: 'possible_output_fields = {"mbol":{"always":True}}\ndef runrule(input_fields, updated_fields):\n\t#return {"shipment_type":"export"}\n\tupdated_fields["bol"] = input_fields["mbol"]'
+      }
+    ]
   }),
   methods: {
     onCmCodeChange (index) {
       const vc = this
       console.log('code snippet: ')
-      console.log(JSON.stringify(vc.codeSnippets[index]))
+      console.log(JSON.stringify(vc.rules_array[index]))
       console.log('after key line isnt how i want')
-      console.log(vc.codeSnippets[index])
+      console.log(vc.rules_array[index])
     },
     addSnippet () {
       const vc = this
-      vc.codeSnippets.push('# Hello Python')
+      vc.rules_array.push({
+        name: 'Default Name',
+        description: 'Edit your description by clicking the button to the right',
+        code: '# This is Python code'
+      })
     },
     removeSnippet (index) {
       const vc = this
-      vc.codeSnippets.splice(index, 1)
+      vc.rules_array.splice(index, 1)
+    },
+    testSnippet (index) {
+      const vc = this
+      console.log(vc.rules_array[index])
+    },
+    editRuleName (index) {
+      const vc = this
+      const newName = prompt('Please type the new name for the rule')
+      vc.rules_array[index].name = newName
+    },
+    editRuleDescription (index) {
+      const vc = this
+      const newDescription = prompt('Please type the new description for the rule')
+      vc.rules_array[index].description = newDescription
     }
   }
 }
