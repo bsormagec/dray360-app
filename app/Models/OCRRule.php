ccd38eb03a6516d293ca9594049fa96d126c302a
Aaron Bryden <abryden@photector.com>
2020-05-28 08:08:46 -0500

    Add Changes for Jetspeed to rules seeder. (#52)
    * Add Changes for Jetson to rules seeder.
    
    * Improve seeder to use OCRRule model
    
    * Update seeder to use Laravel model, not direct db inserts
    
    Co-authored-by: pbnelson <peter@peternelson.com>

--- app/Models/OCRRule.php
+++ app/Models/OCRRule.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class OCRRule extends Model
 {
@@ -12,8 +13,6 @@ class OCRRule extends Model
     const CREATED_AT = 'created_at';
     const UPDATED_AT = 'updated_at';
     protected $dates = ['deleted_at'];
-
-
 
     public $fillable = [
         'name',
--- database/seeds/OCRRulesTableSeeder.php
+++ database/seeds/OCRRulesTableSeeder.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Seeder;
 use Carbon\Carbon;
 
 
-
-
 class OCRRulesTableSeeder extends Seeder
 {
     // class constants
@@ -16,18 +14,25 @@ class OCRRulesTableSeeder extends Seeder
     // two sample rules, from Aaron Bryden
     // rule 1
     CONST RULE1_CODE=<<<PYTHONCODE
-possible_output_fields = {"shipment_type":{"always":True}}
+possible_output_fields = {"shipment_designation":{"always":True}}
 def runrule(input_fields, updated_fields):
-    #return {"shipment_type":"export"}
-    updated_fields["shipment_type"] = "export"
+    #return {"shipment_type":"import"}
+    updated_fields["shipment_designation"] = "import"
 PYTHONCODE;
 
-    // rule2
+    // rule 2
     CONST RULE2_CODE=<<<PYTHONCODE
 possible_output_fields = {"mbol":{"always":True}}
 def runrule(input_fields, updated_fields):
     #return {"shipment_type":"export"}
     updated_fields["bol"] = input_fields["mbol"]
+PYTHONCODE;
+
+    // rule 3
+CONST RULE3_CODE=<<<PYTHONCODE
+possible_output_fields = {"port_ramp_of_origin_address_raw_text":{"always":True}}
+def runrule(input_fields, updated_fields):
+    updated_fields["port_ramp_of_origin_address_raw_text"] = None
 PYTHONCODE;
 
 
@@ -49,12 +54,14 @@ PYTHONCODE;
         $ocrvariantId = $this->seedVariant(5, 'Jetspeed-Import', 'Prenote from Cushing\'s Feight Buyer: Jetspeed');
 
         // create two rules
-        $ocrrule1Id = $this->seedOCRRule('always_export', 'sample rule always_export', self::RULE1_CODE);
+        $ocrrule1Id = $this->seedOCRRule('always_import', 'sample rule always_import', self::RULE1_CODE);
         $ocrrule2Id = $this->seedOCRRule('bol_to_mbol', 'sample rule bol_to_mbol', self::RULE2_CODE);
+        $ocrrule3Id = $this->seedOCRRule('origin_always_null', 'Origin always null', self::RULE3_CODE);
 
         // create two accountvariantrule entries (use sequence)
         $this->seedAccountVariantRuleSequence($accountId, $ocrvariantId, $ocrrule1Id, 1); // rule sequence number 1
         $this->seedAccountVariantRuleSequence($accountId, $ocrvariantId, $ocrrule2Id, 2); // rule sequence number 2
+        $this->seedAccountVariantRuleSequence($accountId, $ocrvariantId, $ocrrule3Id, 3); // rule sequence number 3
     }
 
 
@@ -67,10 +74,8 @@ PYTHONCODE;
      */
     public function seedAddress()
     {
-        $addressId = DB::table('t_addresses')->insertGetId([
-        ]);
-
-        return $addressId;
+        $address = App\Models\Address::create();
+        return $address->id;
     }
 
 
@@ -82,12 +87,14 @@ PYTHONCODE;
      */
     public function seedAccount($name, $addressId)
     {
-        $accountId = DB::table('t_accounts')->insertGetId([
-            'name' => $name,
-            't_address_id' => $addressId
-        ]);
+        $account = App\Models\Account::create(
+            [
+                'name' => $name,
+                't_address_id' => $addressId
+            ]
+        );
 
-        return $accountId;
+        return $account->id;
     }
 
 
@@ -99,13 +106,15 @@ PYTHONCODE;
      */
     public function seedVariant($abbyyVariantId, $abbyyVariantName, $description)
     {
-        $variantId = DB::table('t_ocrvariants')->insertGetId([
-            'abbyy_variant_id' => $abbyyVariantId,
-            'abbyy_variant_name' => $abbyyVariantName,
-            'description' => $description
-        ]);
+        $variant = App\Models\OCRVariant::create(
+            [
+                'abbyy_variant_id' => $abbyyVariantId,
+                'abbyy_variant_name' => $abbyyVariantName,
+                'description' => $description
+            ]
+        );
 
-        return $variantId;
+        return $variant->id;
     }
 
 
@@ -117,13 +126,21 @@ PYTHONCODE;
      */
     public function seedOCRRule($name, $description, $code)
     {
-        $ocrrule1Id = DB::table('t_ocrrules')->insertGetId([
-            'name' => $name,
-            'description' => $description,
-            'code' => $code
-        ]);
+        // $ocrrule1Id = DB::table('t_ocrrules')->insertGetId([
+        //     'name' => $name,
+        //     'description' => $description,
+        //     'code' => $code
+        // ]);
 
-        return $ocrrule1Id;
+        $rule = App\Models\OCRRule::create(
+            [
+                'name' => $name,
+                'description' => $description,
+                'code' => $code
+            ]
+        );
+
+        return $rule->id;
     }
 
 
@@ -135,14 +152,16 @@ PYTHONCODE;
      */
     public function seedAccountVariantRuleSequence($accountId, $ocrvariantId, $ocrruleId, $ruleSequence)
     {
-        $avrId = DB::table('t_account_ocrvariant_ocrrules')->insertGetId([
-            't_account_id' => $accountId,
-            't_ocrvariant_id' => $ocrvariantId,
-            't_ocrrule_id' => $ocrruleId,
-            'rule_sequence' => $ruleSequence
-        ]);
+        $account = App\Models\AccountOCRVariantOCRRule::create(
+            [
+                't_account_id' => $accountId,
+                't_ocrvariant_id' => $ocrvariantId,
+                't_ocrrule_id' => $ocrruleId,
+                'rule_sequence' => $ruleSequence
+            ]
+        );
 
-        return $avrId;
+        return $account->id;
     }
 
 }
