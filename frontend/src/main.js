4db06fc3015db69da5cf33646993802d606fabb1
Esteban <esteban@lean-tech.io>
2020-06-17 17:02:05 -0500

    Report to Sentry

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store/index'
 import vuetify from './plugins/vuetify'
+import * as Sentry from '@sentry/browser'
+import { Vue as VueIntegration } from '@sentry/integrations'
 Vue.config.productionTip = false
+
+Sentry.init({
+  dsn: 'https://8a6b06b12891489eac4e8a979d2d31db@o408962.ingest.sentry.io/5280608',
+  integrations: [new VueIntegration({ Vue, attachProps: true })]
+})
 
 new Vue({
   router,
