6523ac8610eb7bf2337b5ebdacba3d1656751b1c
Esteban <esteban@lean-tech.io>
2020-06-18 14:33:56 -0500

    Change sentry dsn to the one belonging to the company

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,7 +8,7 @@ import { Vue as VueIntegration } from '@sentry/integrations'
 Vue.config.productionTip = false
 
 Sentry.init({
-  dsn: 'https://8a6b06b12891489eac4e8a979d2d31db@o408962.ingest.sentry.io/5280608',
+  dsn: 'https://8084d41e203242d381651d73ddbe7831@o182038.ingest.sentry.io/5281643',
   integrations: [new VueIntegration({ Vue, attachProps: true })]
 })
 
