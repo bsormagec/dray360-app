e49c62ddae6c8087fd2d82dce367e7cdceaf7b45
Santiago Garcia <sgarcia@lean-tech.io>
2020-06-24 15:17:20 -0500

    Add authorization to the sent to tms controller

--- tests/Feature/SendToTmsControllerTest.php
+++ tests/Feature/SendToTmsControllerTest.php
@@ -5,12 +5,14 @@ namespace Tests\Feature;
 use Mockery;
 use Aws\Result;
 use Tests\TestCase;
+use App\Models\User;
 use Aws\MockHandler;
 use App\Models\Order;
 use OrdersTableSeeder;
 use App\Models\Company;
 use App\Models\TMSProvider;
 use Illuminate\Support\Str;
+use Laravel\Sanctum\Sanctum;
 use Illuminate\Http\Response;
 use ProfitToolsCushingSeeder;
 use Aws\Exception\AwsException;
@@ -91,5 +93,35 @@ class SendToTmsControllerTest extends TestCase
             ])
             ->assertJsonFragment(['data' => 'failed-some aws exception'])
             ->assertStatus(Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
+    /** @test */
+    public function it_should_fail_if_not_authorized_to_send_to_tms()
+    {
+        $user = User::whereRoleIs('customer-user')->first();
+        Sanctum::actingAs($user);
+
+        $mockHandler = tap(new MockHandler())
+            ->append(function ($cmd) {
+                return new AwsException('', $cmd, [
+                    'code' => 'failed',
+                    'message' => 'some aws exception',
+                ]);
+            });
+        $snsClient = $this->app['aws']->createClient('sns');
+        $snsClient->getHandlerList()->setHandler($mockHandler);
+
+        $mockAction = Mockery::mock(PublishSnsMessageToSendToTms::class)
+            ->shouldAllowMockingProtectedMethods();
+        $mockAction->shouldNotReceive('getSnsClient');
+        $this->app->instance(PublishSnsMessageToSendToTms::class, $mockAction);
+
+        $this->postJson(route('send-to-tms.store'), [
+                'status' => 'sending-to-wint',
+                'order_id' => $this->order->id,
+                'company_id' => $this->company->id,
+                'tms_provider_id' => $this->tmsProvider->id,
+            ])
+            ->assertStatus(Response::HTTP_FORBIDDEN);
     }
 }
