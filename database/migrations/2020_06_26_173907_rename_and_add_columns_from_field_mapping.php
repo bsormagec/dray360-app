1ffe37159276e21f5ba43a114feb2f070fd9483a
Santiago Garcia <sgarcia@lean-tech.io>
2020-06-29 13:00:13 -0500

    Add missing columns to the migration, model, and factory

--- database/migrations/2020_06_26_173907_rename_and_add_columns_from_field_mapping.php
+++ database/migrations/2020_06_26_173907_rename_and_add_columns_from_field_mapping.php
@@ -18,6 +18,21 @@ class RenameAndAddColumnsFromFieldMapping extends Migration
             $table->renameColumn('bol', 'bill_of_lading');
 
             $table->string('carrier', 64)->nullable();
+            $table->string('bill_charge', 64)->nullable();
+            $table->string('bill_comment', 64)->nullable();
+            $table->string('line_haul', 64)->nullable();
+            $table->string('rate_box', 64)->nullable();
+            $table->string('fuel_surcharge', 64)->nullable();
+            $table->string('total_accessorial_charges', 64)->nullable();
+            $table->string('equipment_provider', 64)->nullable();
+            $table->string('actual_destination', 64)->nullable();
+            $table->string('actual_origin', 64)->nullable();
+            $table->string('customer_number', 64)->nullable();
+            $table->string('expedite', 64)->nullable();
+            $table->string('load_number', 64)->nullable();
+            $table->string('purchase_order_number', 64)->nullable();
+            $table->string('release_number', 64)->nullable();
+            $table->string('ship_comment', 64)->nullable();
         });
 
         Schema::table('t_order_line_items', function (Blueprint $table) {
@@ -36,7 +51,24 @@ class RenameAndAddColumnsFromFieldMapping extends Migration
             $table->renameColumn('seal_number', 'seal_number_list');
             $table->renameColumn('bill_of_lading', 'bol');
 
-            $table->dropColumn('carrier');
+            $table->dropColumn([
+                'carrier',
+                'bill_charge',
+                'bill_comment',
+                'line_haul',
+                'rate_box',
+                'fuel_surcharge',
+                'total_accessorial_charges',
+                'equipment_provider',
+                'actual_destination',
+                'actual_origin',
+                'customer_number',
+                'expedite',
+                'load_number',
+                'purchase_order_number',
+                'release_number',
+                'ship_comment',
+            ]);
         });
 
         Schema::table('t_order_line_items', function (Blueprint $table) {
