a00201a6473ad810ca769db6c4777176d3b3e13b
Peter Nelson <pbnelson@users.noreply.github.com>
2020-08-04 10:06:46 -0500

    Add new address_locationname_override table, migration, seed (#176)
    * Add new address_locationname_override table, migration, seed
    
    * Use updateOrCreate to facilitate tweaking these values in future

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(ProfitToolsCushingSeeder::class);
+        $this->call(AddressLocationnameOverrideSeeder::class);
         $this->call(LaratrustSeeder::class);
     }
 }
