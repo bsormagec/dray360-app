df14b2a35e1d62dbc881266ec2ad70901bb37393
Esteban <esteban@lean-tech.io>
2020-08-31 15:04:42 -0500

    add api calls

---
+++ frontend/src/store/api_calls/users.js
@@ -0,0 +1,9 @@
+import axios from '@/store/api_calls/config/axios'
+
+export const getUsers = async () => axios.ext.get('/api/users').then(data => [undefined, data.data]).catch(e => [e])
+
+export const deleteUser = async (id) => axios.ext.delete(`/api/users/${id}`).then(data => [undefined, data.data]).catch(e => [e])
+
+export const editUser = async (id) => axios.ext.put(`/api/users/${id}`).then(data => [undefined, data.data]).catch(e => [e])
+
+export const addUser = async (userData) => axios.ext.post('/api/users/', userData).then(data => [undefined, data.data]).catch(e => [e])
