5be880d7b54a29aa4729549938a9a01a3a6c178a
sgarcia-leantech <65971173+sgarcia-leantech@users.noreply.github.com>
2020-09-03 08:48:57 -0500

    Extend session duration and add the environment in the redis,horizon and sesion prefix (#222)

--- config/database.php
+++ config/database.php
@@ -133,7 +133,10 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env(
+                'REDIS_PREFIX',
+                Str::slug(env('APP_NAME', 'laravel'), '_').env('APP_ENV').'_database_'
+            ),
         ],
 
         'default' => [
