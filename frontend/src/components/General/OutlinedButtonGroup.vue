99aa769d670b380cd7a4f11dd7e384ed6d387f7a
Esteban <esteban@lean-tech.io>
2020-09-17 19:30:59 -0500

    Start working on new button component

---
+++ frontend/src/components/General/OutlinedButtonGroup.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <!-- <v-btn-toggle v-model="toggle_none">
+      <v-btn
+        class="ma-2"
+        outlined
+        color="indigo"
+      >
+        Outlined Button
+      </v-btn>
+      <v-btn
+        class="ma-2"
+        outlined
+        color="indigo"
+      >
+        i
+      </v-btn>
+    </v-btn-toggle> -->
+    <v-row>
+      <v-btn
+        outlined
+        color="indigo"
+      >
+        View
+      </v-btn>
+      <v-btn
+        class='selector'
+        outlined
+        color="indigo"
+      >
+        ▼
+      </v-btn>
+    </v-row>
+  </div>
+</template>
+<script>
+export default {
+  name: 'OutlinedButtonGroup'
+}
+</script>
+<style scoped>
+.selector{
+    padding-left: -10px
+}
+</style>
