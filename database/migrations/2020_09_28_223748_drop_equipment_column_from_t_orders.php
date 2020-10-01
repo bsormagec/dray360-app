7c164e9c888cb215ee3c1750a950126b0cccff9c
pbnelson <peter@peternelson.com>
2020-10-01 16:43:56 -0500

    Updating migrations

--- database/migrations/2020_09_28_223748_drop_equipment_column_from_t_orders.php
+++ database/migrations/2020_09_28_223748_drop_equipment_column_from_t_orders.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class DropEquipmentColumnFromTOrders extends Migration
 {
@@ -13,11 +13,43 @@ class DropEquipmentColumnFromTOrders extends Migration
      */
     public function up()
     {
+        // drop t_orders.equipment
         if (Schema::hasColumn('t_orders', 'equipment')) {
             Schema::table('t_orders', function (Blueprint $table) {
                 $table->dropColumn('equipment');
             });
         }
+
+        // drop t_orders.owner_or_ss_company
+        if (Schema::hasColumn('t_orders', 'owner_or_ss_company')) {
+            Schema::table('t_orders', function (Blueprint $table) {
+                $table->dropColumn('owner_or_ss_company');
+            });
+        }
+
+        // add t_orders.equipment_type_verified
+        Schema::table('t_orders', function (Blueprint $table) {
+            $table->boolean('equipment_type_verified')->nullable();
+        });
+
+        // update new formula for t_equipment_types.equipment_display column
+        // to prefix the equipment_owner
+        $storedColumnQuery = <<<'mysql'
+            concat(
+                equipment_owner,
+                if (equipment_owner = '', '', ' '),
+                if (row_type = 'combined',
+                    equipment_type_and_size,
+                    if (row_type = 'separate',
+                        concat(equipment_type, ' ',equipment_size),
+                        ''
+                    )
+                )
+            )
+        mysql;
+        Schema::create('t_equipment_types', function (Blueprint $table) {
+            $table->string('equipment_display')->storedAs($storedColumnQuery);
+        });
     }
 
     /**
@@ -27,8 +59,35 @@ class DropEquipmentColumnFromTOrders extends Migration
      */
     public function down()
     {
+        // restore t_orders.equipment_type
         Schema::table('t_orders', function (Blueprint $table) {
             $table->string('equipment_type', 64)->nullable();
+        });
+
+        // restore t_orders.owner_or_ss_company
+        Schema::table('t_orders', function (Blueprint $table) {
+            $table->string('owner_or_ss_company', 64)->nullable();
+        });
+
+        // drop t_orders.equipment_type_verified
+        if (Schema::hasColumn('t_orders', 'equipment_type_verified')) {
+            Schema::table('t_orders', function (Blueprint $table) {
+                $table->dropColumn('equipment_type_verified');
+            });
+        }
+
+        // restore original t_equipment_types.equipment_display column
+        $storedColumnQuery = <<<'mysql'
+            if (row_type = 'combined',
+                equipment_type_and_size,
+                if (row_type = 'separate',
+                    concat(equipment_type, ' ',equipment_size),
+                    ''
+                )
+            )
+        mysql;
+        Schema::create('t_equipment_types', function (Blueprint $table) {
+            $table->string('equipment_display')->storedAs($storedColumnQuery);
         });
     }
 }
