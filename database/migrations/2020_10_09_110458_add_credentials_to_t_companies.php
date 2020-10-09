edbefbe3b82252bfadcc934a7c75787cbb0e642b
pbnelson <peter@peternelson.com>
2020-10-09 06:51:28 -0500

    Add migrations for storing encrypted ripcms & blackfly credentials

---
+++ database/migrations/2020_10_09_110458_add_credentials_to_t_companies.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCredentialsToTCompanies extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('t_companies', function (Blueprint $table) {
+            $table->text('blackfly_token')->nullable();
+            $table->string('ripcms_username', 128)->nullable();
+            $table->string('ripcms_password', 64)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasColumn('t_companies', 'blackfly_token')) {
+            Schema::table('t_companies', function (Blueprint $table) {
+                $table->dropColumn('blackfly_token');
+            });
+        }
+        if (Schema::hasColumn('t_companies', 'ripcms_username')) {
+            Schema::table('t_companies', function (Blueprint $table) {
+                $table->dropColumn('ripcms_username');
+            });
+        }
+        if (Schema::hasColumn('t_companies', 'ripcms_password')) {
+            Schema::table('t_companies', function (Blueprint $table) {
+                $table->dropColumn('ripcms_password');
+            });
+        }
+
+    }
+}
