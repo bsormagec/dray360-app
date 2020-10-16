b88a1dd3a979effe699c9cfa124861a12803e32f
jorge-gutierrez-lt <71669894+jorge-gutierrez-lt@users.noreply.github.com>
2020-10-16 09:55:12 -0500

    Bugfix/order details fix (#291)
    * Fix order details js errors
    
    * fix minor styles
    
    * Change order-form initialState order
    
    Co-authored-by: Santiago Garcia <sgarcia@lean-tech.io>
    Co-authored-by: mauricio <mmontoya@lean-tech.io>

--- frontend/src/components/FormFields/FormFieldAddressSwitchVerify.vue
+++ frontend/src/components/FormFields/FormFieldAddressSwitchVerify.vue
@@ -2,7 +2,10 @@
   <!--  eslint-disable vue/no-v-html -->
   <div class="form-field-element-modal-address">
     <div class="address-book-modal">
-      <span class="address-book-modal__title"><strong>{{ label }}</strong></span>
+      <span
+        v-if="label !== ''"
+        class="address-book-modal__title"
+      ><strong>{{ label }}</strong></span>
 
       <div class="address-book-modal__body">
         <div
@@ -78,7 +81,7 @@ export default {
   },
 
   props: {
-    label: { type: String, required: true },
+    label: { type: String, required: false, default: '' },
     verified: { type: Boolean, required: true },
     recognizedText: { type: String, default: '' },
     matchedAddress: { required: true },
