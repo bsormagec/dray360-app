dbe0d3c389cc103d5312fa1212ef015ad6710fa4
mauricio <mmontoya@lean-tech.io>
2020-10-21 16:21:14 -0500

    add division code table, model and seeder

---
+++ database/migrations/2020_10_19_204110_create_t_division_code_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTDivisionCodeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t_division_code', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('t_company_id')->references('id')->on('t_companies');
+            $table->foreign('t_tms_provider_id')->references('id')->on('t_tms_providers');
+            $table->varchar('division_code', 32);
+            $table->string('division_name', 128);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t_division_code');
+    }
+}
