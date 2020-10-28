2058515d8b154286eedca07d4db83d27a54ded15
esteban-lean-tech <evargas@lean-tech.io>
2020-10-28 16:49:10 -0500

    Uncomment tests

--- frontend/cypress/integration/user-mgmt/delete-user.spec.js
+++ frontend/cypress/integration/user-mgmt/delete-user.spec.js
@@ -1,34 +1,34 @@
-// describe('Delete user', function () {
-//   beforeEach(() => {
-//     cy.fixture('auth/tenant.json').as('tenant')
-//     cy.fixture('auth/user.json').as('user')
-//     cy.fixture('user-mgmt/roles.json').as('roles')
-//     cy.fixture('user-mgmt/company.json').as('company')
-//     cy.fixture('user-mgmt/user-list.json').as('users')
-//     cy.fixture('user-mgmt/edited-user.json').as('editedUser')
-//   })
+describe('Delete user', function () {
+  beforeEach(() => {
+    cy.fixture('auth/tenant.json').as('tenant')
+    cy.fixture('auth/user.json').as('user')
+    cy.fixture('user-mgmt/roles.json').as('roles')
+    cy.fixture('user-mgmt/company.json').as('company')
+    cy.fixture('user-mgmt/user-list.json').as('users')
+    cy.fixture('user-mgmt/edited-user.json').as('editedUser')
+  })
 
-//   it('deletes a user successfully', function () {
-//     cy.server()
+  it('deletes a user successfully', function () {
+    cy.server()
 
-//     cy.route({ url: '**/api/user', response: this.user })
+    cy.route({ url: '**/api/user', response: this.user })
 
-//     cy.route({ url: '**/api/current-tenant', response: this.tenant })
+    cy.route({ url: '**/api/current-tenant', response: this.tenant })
 
-//     cy.route({ url: '**/api/users', response: this.users })
+    cy.route({ url: '**/api/users', response: this.users })
 
-//     cy.route({ url: '**/api/roles', response: this.roles })
+    cy.route({ url: '**/api/roles', response: this.roles })
 
-//     cy.route({ url: '**/api/companies/2', response: this.company })
+    cy.route({ url: '**/api/companies/2', response: this.company })
 
-//     cy.visit('http://localhost:8080/user/dashboard')
+    cy.visit('http://localhost:8080/user/dashboard')
 
-//     cy.wait(1000)
+    cy.wait(1000)
 
-//     cy.route({ method: 'DELETE', url: '**/api/users/**', response: this.users })
+    cy.route({ method: 'DELETE', url: '**/api/users/**', response: this.users })
 
-//     cy.get('tbody>tr>td>button').eq(1).click()
+    cy.get('tbody>tr>td>button').eq(1).click()
 
-//     cy.url().should('include', 'user/dashboard')
-//   })
-// })
+    cy.url().should('include', 'user/dashboard')
+  })
+})
--- frontend/cypress/integration/user-mgmt/edit-user.spec.js
+++ frontend/cypress/integration/user-mgmt/edit-user.spec.js
@@ -1,36 +1,36 @@
-// describe('Edit user', function () {
-//   beforeEach(() => {
-//     cy.fixture('auth/tenant.json').as('tenant')
-//     cy.fixture('auth/user.json').as('user')
-//     cy.fixture('user-mgmt/roles.json').as('roles')
-//     cy.fixture('user-mgmt/company.json').as('company')
-//     cy.fixture('user-mgmt/user-list.json').as('users')
-//     cy.fixture('user-mgmt/edited-user.json').as('editedUser')
-//   })
+describe('Edit user', function () {
+  beforeEach(() => {
+    cy.fixture('auth/tenant.json').as('tenant')
+    cy.fixture('auth/user.json').as('user')
+    cy.fixture('user-mgmt/roles.json').as('roles')
+    cy.fixture('user-mgmt/company.json').as('company')
+    cy.fixture('user-mgmt/user-list.json').as('users')
+    cy.fixture('user-mgmt/edited-user.json').as('editedUser')
+  })
 
-//   it('edits a user successfully', function () {
-//     cy.server()
+  it('edits a user successfully', function () {
+    cy.server()
 
-//     cy.route({ url: '**/api/user', response: this.user })
+    cy.route({ url: '**/api/user', response: this.user })
 
-//     cy.route({ url: '**/api/current-tenant', response: this.tenant })
+    cy.route({ url: '**/api/current-tenant', response: this.tenant })
 
-//     cy.route({ url: '**/api/roles', response: this.roles })
+    cy.route({ url: '**/api/roles', response: this.roles })
 
-//     cy.route({ url: '**/api/companies/2', response: this.company })
+    cy.route({ url: '**/api/companies/2', response: this.company })
 
-//     cy.visit('http://localhost:8080/user/dashboard/edit-user/1')
+    cy.visit('http://localhost:8080/user/dashboard/edit-user/1')
 
-//     cy.get('[data-cy=name-input]').type('Bill Brasky')
+    cy.get('[data-cy=name-input]').type('Bill Brasky')
 
-//     cy.get('[data-cy=email-input]').type('bill@example.com')
+    cy.get('[data-cy=email-input]').type('bill@example.com')
 
-//     cy.route({ method: 'PUT', url: '**/api/users/**', response: this.editedUser })
+    cy.route({ method: 'PUT', url: '**/api/users/**', response: this.editedUser })
 
-//     cy.route({ method: 'GET', url: '**/api/users', response: this.users })
+    cy.route({ method: 'GET', url: '**/api/users', response: this.users })
 
-//     cy.get('[data-cy=save-button]').click()
+    cy.get('[data-cy=save-button]').click()
 
-//     cy.url().should('include', 'user/dashboard')
-//   })
-// })
+    cy.url().should('include', 'user/dashboard')
+  })
+})
