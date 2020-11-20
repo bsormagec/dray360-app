cf46b4d76757659eb41a8b249a95dc7a97c73c2c
pbnelson <peter@peternelson.com>
2020-11-20 15:01:52 -0600

    Fix rollback bug

--- database/migrations/2020_11_20_180025_add_cutoff_datetime_to_t_orders.php
+++ database/migrations/2020_11_20_180025_add_cutoff_datetime_to_t_orders.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 class AddCutoffDatetimeToTOrders extends Migration
 {
     /**
@@ -27,8 +28,8 @@ class AddCutoffDatetimeToTOrders extends Migration
     public function down()
     {
         Schema::table('t_orders', function (Blueprint $table) {
-            dropColumnIfExists($tableName, 'cutoff_date');
-            dropColumnIfExists($tableName, 'cutoff_time');
+            $table->dropColumnIfExists('cutoff_date');
+            $table->dropColumnIfExists('cutoff_time');
         });
     }
 }
