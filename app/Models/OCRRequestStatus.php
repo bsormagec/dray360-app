075889aef689c9e8d8386a08f3329c668fad0b26
pbnelson <peter@peternelson.com>
2020-12-09 17:32:47 -0600

    Update tms support to use correct status for chainio

--- app/Models/OCRRequestStatus.php
+++ app/Models/OCRRequestStatus.php
@@ -43,6 +43,13 @@ class OCRRequestStatus extends Model
     SUCCESS_UPDATING_TO_WINT = 'success-updating-to-wint',
     SHIPMENT_UPDATED_BY_WINT = 'shipment-updated-by-wint',
     SHIPMENT_NOT_UPDATED_BY_WINT = 'shipment-not-updated-by-wint',
+
+    SENDING_TO_CHAINIO = 'sending-to-chainio',
+    SUCCESS_SENDING_TO_CHAINIO = 'success-sending-to-chainio',
+    FAILURE_SENDING_TO_CHAINIO = 'failure-sending-to-chainio',
+    SHIPMENT_CREATED_BY_CHAINIO = 'shipment-created-by-chainio',
+    SHIPMENT_NOT_CREATED_BY_CHAINIO = 'shipment-not-created-by-chainio',
+
     UPDATES_PRIOR_ORDER = 'updates-prior-order',
     UPDATED_BY_SUBSEQUENT_ORDER = 'updated-by-subsequent-order',
 
@@ -76,14 +83,17 @@ class OCRRequestStatus extends Model
 
         self::SENDING_TO_WINT                    => 'Sending to TMS',
         self::UPDATING_TO_WINT                   => 'Sending to TMS',
+        self::SENDING_TO_CHAINIO                 => 'Sending to TMS',
 
         self::SUCCESS_SENDING_TO_WINT            => 'Sent to TMS',
         self::SUCCESS_UPDATING_TO_WINT           => 'Sent to TMS',
+        self::SUCCESS_SENDING_TO_CHAINIO         => 'Sent to TMS',
 
         self::SHIPMENT_CREATED_BY_WINT           => 'Accepted by TMS',
         self::SHIPMENT_UPDATED_BY_WINT           => 'Accepted by TMS',
         self::SUCCESS_IMAGEUPLOADING_TO_BLACKFLY => 'Accepted by TMS',
         self::UNTRIED_IMAGEUPLOADING_TO_BLACKFLY => 'Accepted by TMS',
+        self::SHIPMENT_CREATED_BY_CHAINIO        => 'Accepted by TMS',
 
         self::SHIPMENT_NOT_UPDATED_BY_WINT       => 'TMS Warning',
         self::FAILURE_UPDATING_TO_WINT           => 'TMS Warning',
@@ -91,6 +101,8 @@ class OCRRequestStatus extends Model
 
         self::SHIPMENT_NOT_CREATED_BY_WINT       => 'TMS Error',
         self::FAILURE_SENDING_TO_WINT            => 'TMS Error',
+        self::SHIPMENT_NOT_CREATED_BY_CHAINIO    => 'TMS Error',
+        self::FAILURE_SENDING_TO_CHAINIO         => 'TMS Error',
     ];
 
     public $table = 't_job_state_changes';
