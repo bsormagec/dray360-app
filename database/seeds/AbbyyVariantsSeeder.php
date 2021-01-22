0407b32c215f58faaffa869c5a2a919d8bef705b
pbnelson <peter@peternelson.com>
2021-01-22 14:33:24 -0600

    Update abbyy seeder and csv

--- database/seeds/AbbyyVariantsSeeder.php
+++ database/seeds/AbbyyVariantsSeeder.php
@@ -22,7 +22,7 @@ class OCRVariantsSeeder extends Seeder
 {
     const INPUT_FILES = [
         [
-            "FILENAME" => 'database/seeds/abbyy_variants_20201218.csv'
+            "FILENAME" => 'database/seeds/abbyy_variants_20210122.csv'
         ],
     ];
 
@@ -41,7 +41,7 @@ class OCRVariantsSeeder extends Seeder
             foreach ($variantsData as $csvRow) {
                 $variantRow = $this->getVariantRowFromCsvRow($csvRow);
                 $variantLookup = [
-                    'abbyy_variant_id' => $variantRow['abbyy_variant_id'],
+                    # 'abbyy_variant_id' => $variantRow['abbyy_variant_id'],
                     'abbyy_variant_name' => $variantRow['abbyy_variant_name'],
                 ];
                 $rownum = $variantRow['rownum'];
