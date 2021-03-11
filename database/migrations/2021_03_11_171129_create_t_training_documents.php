419f51256e4a001fd8c63c01fd8629ec82f6d798
pbnelson <peter@peternelson.com>
2021-03-11 12:33:56 -0600

    Update migration

--- database/migrations/2021_03_11_171129_create_t_training_documents.php
+++ database/migrations/2021_03_11_171129_create_t_training_documents.php
@@ -15,16 +15,16 @@ class CreateTTrainingDocuments extends Migration
     {
         Schema::create('t_training_documents', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->string('type', 128);
-            $table->string('s3uri', 1024)->index();
+            $table->string('s3uri', 1024)->charset('utf8')->index();
             $table->string('sha256sum', 64)->index();
-            $table->datetime('discovered_at')->nullable();
             $table->json('document_metadata')->nullable();
             $table->json('external_references')->nullable();
             $table->json('extracted_features_list')->nullable();
             $table->json('labels')->nullable();
-            $table->json('document_metadata')->nullable();
+            $table->datetime('discovered_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
