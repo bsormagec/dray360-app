ccb6ac71b90cc74eb759079daa33758eb76eb483
Daniel J. Bell <46620272+danieljustinbell@users.noreply.github.com>
2021-09-14 15:27:18 -0500

    DO-257 LockeTransportation equipment types (#843)
    Co-authored-by: danieljustinbell <dbell@dray360.com>

--- database/seeders/EquipmentTypesSeeder.php
+++ database/seeders/EquipmentTypesSeeder.php
@@ -20,6 +20,17 @@ class EquipmentTypesSeeder extends Seeder
      * add the specific companies to actually import.
      */
     const ALL_COMPANIES = [
+        'LockeTransportation' => [
+            'file' => 'seeders/LockeTransportation_EquipmentLeaseTypes.20210914.xlsx',
+            'columns' => [
+                'tms_equipment_id' => 'id',
+                'equipment_owner' => 'line',
+                'equipment_type_and_size' => 'type',
+                'equipment_size' => 'equipmentlength',
+                'scac' => 'scac',
+                'line_prefix_list' => 'lineprefix',
+            ],
+        ],
         'MutualExpress' => [
             'file' => 'seeders/MutualExpress_EquipmentLeaseTypes.20210910.xlsx',
             'columns' => [
@@ -321,7 +332,7 @@ class EquipmentTypesSeeder extends Seeder
      * there is usually no reason to re-import more than once.
      */
     const TO_BE_IMPORTED = [
-        'PFS' => self::ALL_COMPANIES['PFS']
+        'LockeTransportation' => self::ALL_COMPANIES['LockeTransportation']
     ];
 
     /**
